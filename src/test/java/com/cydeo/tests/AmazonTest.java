9b14d6a95c4d927a23f53c066ced237cf3d6039a
mhikmetozcan <mhikmetozcan@gmail.com>
2022-05-31 09:21:21 +0200

    First navigation to Amazon.com

--- src/test/java/com/cydeo/tests/AmazonTest.java
+++ src/test/java/com/cydeo/tests/AmazonTest.java
@@ -1,4 +1,28 @@
 package com.cydeo.tests;
 
+import com.cydeo.utils.ConfigurationReader;
+import com.cydeo.utils.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class AmazonTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void browserSetup(){
+        String browserType = ConfigurationReader.getProperty("browser");
+        driver = WebDriverFactory.getDriver(browserType);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void getToAmazonDotCom(){
+        driver.get("https://www.amazon.com");
+    }
+
 }
